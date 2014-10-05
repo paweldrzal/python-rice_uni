8c8ef57a8e28f6a34cc8b27fb752aed1d96a8336
Pawel <pd.paweldrzal@gmail.com>
2014-10-05 22:40:35 +0200

    pset2 guess_number update

--- guess_number.py
+++ guess_number.py
@@ -1,4 +1,4 @@
-te for "Guess the number" mini-project
+#"Guess the number" mini-project
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
 import simplegui
